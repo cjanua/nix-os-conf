7d792eff44f174a0e7200e733d04b0fbba645487
Chris Januario <christophercoelhoj@gmail.com>
2024-03-15 19:24:01 -0400

    Add NixOS configuration file

---
+++ configuration.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix> ];
+
+  networking.hostName = "ChrisStockApp-nixos";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  time.timeZone = "America/New_York";
+  
+  
+  environment.systemPackages = with pkgs; [
+    neofetch
+    neovim  
+    wget
+    gh
+    git
+  ];
+  users.users.chrisj = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable 'sudo' for the user.
+  };  
+}
